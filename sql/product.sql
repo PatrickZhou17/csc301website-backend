cba4d0958c67b18c4d3c6fe3ac727aab818811e3
Patrick Zhou <44577726+PatrickZhou17@users.noreply.github.com>
2022-10-16 14:02:10 -0400

    delete unnecessary part

--- sql/product.sql
+++ sql/product.sql
@@ -4,9 +4,6 @@ CREATE TABLE `product_info` (
      `name` varchar(50) NOT NULL COMMENT 'name',
      `price` float NOT NULL COMMENT 'price',
      `total` int DEFAULT 0 COMMENT 'amount',
-     `created_at` datetime DEFAULT NULL COMMENT 'create time',
-     `updated_at` datetime DEFAULT NULL COMMENT 'update time',
-     `deleted_at` datetime DEFAULT NULL COMMENT 'delete time',
-     PRIMARY KEY (`id`),
+
      UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='list of items';
