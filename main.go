569086ba4c64c7ef31e9c6d4df126a58953541f3
Patrick Zhou <44577726+PatrickZhou17@users.noreply.github.com>
2022-10-16 22:12:08 -0400

    update database route

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	router.Use(gin.Logger())
 	dbCfg := config.DatabaseConfig{
 		ConnectUrls: config.DBConnUrls{
-			Master: "uu23w23lguh6ef2q:mfoybymlxryqtpmw@tcp(r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/tz4sjutxf9pddu6t?charset=utf8mb4&parseTime=True&loc=Local",
+			Master: "czvfwkhi1d47gx8z:rmi4g21wg0xx4917@tcp(r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/ilib8n1ztxr3lpbp?charset=utf8mb4&parseTime=True&loc=Local",
 		},
 		MaxIdle:  10,
 		MaxOpen:  100,
